10fa3747eeda4a1307df5f7885142debc2d4fbfe
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2024-11-04 18:36:24 -0700

    remove gfx_tools_rd

--- gfx_tools_rd/IgnoreCaseComparer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-struct IgnoreCaseComparer {
-    bool operator()(const std::string &str, const std::string &str2) const noexcept {
-        return _strnicmp(str.c_str(), str2.c_str(), str.size() + 1) < 0;
-    }
-};
