17bc213acfff3fa2ab10bb5bc422627128f71aa0
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2024-11-29 19:10:50 -0700

    clarify in the wording that it must be a full install, just in case anyone gets ideas

--- M4Revolution/Work.cpp
+++ M4Revolution/Work.cpp
@@ -237,17 +237,17 @@ namespace Work {
 		bool foundInstalled = setPath(path);
 
 		if (foundInstalled) {
-			consoleLog("An install of Myst IV: Revelation was found at this path:");
+			consoleLog("A full install of Myst IV: Revelation was found at this path:");
 			consoleLog(path.string().c_str(), 2);
 
 			foundInstalled = consoleBool("Is this the path to the install you would like to modify?", true);
 		} else {
-			consoleLog("No install of Myst IV: Relevation was found. ", false);
+			consoleLog("A full install of Myst IV: Relevation was not found. ", false);
 		}
 
 		if (!foundInstalled) {
-			while (!setPath(consoleString("Please enter the path to Myst IV: Relevation."))) {
-				consoleLog("No install of Myst IV: Relevation was found at this path. ", false);
+			while (!setPath(consoleString("Please enter the path to a full install of Myst IV: Relevation."))) {
+				consoleLog("A full install of Myst IV: Relevation was not found at this path. ", false);
 			}
 		}
 	}
