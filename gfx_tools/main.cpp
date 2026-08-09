b46cd4955f477fe8c06bfbf8d51a340c1c7b504c
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2026-08-09 00:20:25 -0600

    minor tweak

--- gfx_tools/main.cpp
+++ gfx_tools/main.cpp
@@ -229,7 +229,7 @@ _VCRT_ALLOCATOR void* __CRTDECL operator new(
 	std::align_val_t _Al
 	) {
 	size_t padding = (size_t)_Al - 1;
-	size_t offset = padding + sizeof(void*);
+	size_t offset = sizeof(void*) + padding;
 
 	try {
 		Validate::overflow(offset, _Size);
